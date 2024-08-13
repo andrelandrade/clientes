18d536085ca39b30f9cc82daac5d29c4b0288b76
André Luis de Andrade <andreandrade042@gmail.com>
2024-08-13 19:43:00 -0300

    Initial commit

---
+++ src/main/kotlin/com/example/clientes/ClientesApplication.kt
@@ -0,0 +1,11 @@
+package com.example.clientes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClientesApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClientesApplication>(*args)
+}
